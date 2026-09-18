d380d5d1f2b106b166ec9069dba098d7002587c1
williamVM4 <enriquemancia25@gmail.com>
2026-09-17 20:54:23 -0600

    Feature: k8s hands on

---
+++ k8s-hands-on/backend/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	W.Header().Set("Content-Type", "application/json")
+	w.Write([]byte('{"message": "hello world"}'))
+}
+
+func main() {
+	http.HandleFunc("/", ServeHTTP)
+	http.ListenAndServe(":9090", nil)
+}
